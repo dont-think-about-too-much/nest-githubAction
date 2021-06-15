82bda417ccbfed75ceedb8406a796a3cfb35232d
moongq <18knee@gmail.com>
2021-06-15 18:58:09 +0900

    df

--- scripts/afterInstall.sh
+++ scripts/afterInstall.sh
@@ -10,6 +10,7 @@ echo "이동함"
 CURRENT_PID=$(sudo lsof -t -i:3000)
 
 echo "현재 아이디"
+echo "$CURRENT_PID"
 
 sudo kill -9 $CURRENT_PID
 
@@ -18,3 +19,5 @@ echo "죽임"
 echo "기존에 있던 3000포트 삭제"
 
 node dist/main.js
+
+exit
