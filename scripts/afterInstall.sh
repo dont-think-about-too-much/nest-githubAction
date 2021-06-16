5c21b19cd15f4efb37b832201a2f374c0251d63b
moongq <18knee@gmail.com>
2021-06-16 10:33:53 +0900

    pm2로 설정 변경

--- scripts/afterInstall.sh
+++ scripts/afterInstall.sh
@@ -9,15 +9,15 @@ echo "이동함"
 
 CURRENT_PID=$(sudo lsof -t -i:3000)
 
-echo "현재 아이디"
-echo "$CURRENT_PID"
+# echo "현재 아이디"
+# echo "$CURRENT_PID"
 
-sudo kill -9 $CURRENT_PID
+# sudo kill -9 $CURRENT_PID
 
-echo "죽임"
+# echo "죽임"
 
-echo "기존에 있던 3000포트 삭제"
+# echo "기존에 있던 3000포트 삭제"
 
-node dist/main.js
+pm2 restart dist/main.js
 
-exit
+echo "pm2 재시작 완료"
