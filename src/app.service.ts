9ddf6a46bdec714b1ede13c9a1f17ad81de374ba
moongq <18knee@gmail.com>
2021-06-16 10:53:14 +0900

    pm2로 설정 변경

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,7 +3,7 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!3';
-    
+    return 'Hello World!3444';
+
   }
 }
